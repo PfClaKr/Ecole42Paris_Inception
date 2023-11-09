18a93723b48d501c7db40f807df4b729f883478e
Yugeon CHUN / 전유건 <85597496+PfClaKr@users.noreply.github.com>
2023-11-09 22:48:24 +0100

    Add files via upload

--- for2/srcs/requirements/wordpress/tools/entrypoint.sh
+++ for2/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,16 +1,25 @@
-#! /bin.bash
+#! /bin/bash
 
 service php7.3-fpm start;
-echo "listen = 0.0.0.0:9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+#echo "listen = 0.0.0.0:9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	curl -O "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
 
 	wp core download --allow-root --path=/var/www/html/
-	wp core config --dbname=wordpress_db --dbuser=ychun --dbpass=1 --dbhost=mariadb --dbprefix=wp_ --allow-root --path=/var/www/html/
-	wp core install --url=https://ychun.42.fr --title="Ychun inception" --admin_user=pfcla --admin_password=1 --admin_email=ychun@42.fr --allow-root --path=/var/www/html/
+	echo "4"
+	cd /var/www/html
+	cp wp-config-sample.php wp-config.php
+	sed -i "s/database_name_here/wordpress_db/g" wp-config.php
+	sed -i "s/username_here/ychun/g" wp-config.php
+	sed -i "s/password_here/1/g" wp-config.php
+	sed -i "s/localhost/172.31.0.11/g" wp-config.php
+#--dbname="wordpress_db" --dbuser="ychun" --dbpass="1" --dbhost="mariadb" --dbprefix="wp_" --allow-root --path=/var/www/html/
+	echo "5"
+	wp core install --url="https://ychun.42.fr" --title="Ychun inception" --admin_user="pfcla" --admin_password=1 --admin_email="ychun@42.fr" --allow-root --path=/var/www/html/
+	echo "6"
 fi
 
 service php7.3-fpm stop;
